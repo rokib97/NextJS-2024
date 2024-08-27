35c76c66323cab7e35ccf478e8121ee3a64cfaef
rokib97 <hasanrokib1997@gmail.com>
2024-08-27 23:42:42 +0600

    create store

--- blogiz-starter-pack/src/app/layout.tsx
+++ blogiz-starter-pack/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import Providers from "@/lib/Providers";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
@@ -22,12 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
-      <body className={roboto.className}>
-        <Header />
-        <div className="min-h-screen">{children}</div>
-        <Footer />
-      </body>
-    </html>
+    <Providers>
+      <html lang="en" data-theme="light">
+        <body className={roboto.className}>
+          <Header />
+          <div className="min-h-screen">{children}</div>
+          <Footer />
+        </body>
+      </html>
+    </Providers>
   );
 }
